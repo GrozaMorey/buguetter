3701b34bf40817f463018cf32586d70f7f64c224
GrozaMorey <yorik294@gmail.com>
2022-09-22 07:32:36 +0300

    change code structure

--- index.py
+++ index.py
@@ -1,12 +1,9 @@
-from flask import Flask,render_template,url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import psycopg2.extras
 from flask import session
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/User'
@@ -22,24 +19,36 @@ DB_TABLE = "users2"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
 
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(40), nullable= False)
+    login = db.Column(db.String(40), nullable=False)
     name = db.Column(db.String(40), nullable=False)
-    password = db.Column(db.String(255), nullable= False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, login, name, password):
         self.login = login
         self.name = name
         self.password = password
 
+def cursor_select(column, arg):
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(f"SELECT * FROM {DB_TABLE} WHERE {column} = '{arg}'")
+    account = cursor.fetchone()
+    return account
+
+def add_user(name,login,_hashed_password):
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(f"INSERT INTO {DB_TABLE} (login, name, password) VALUES ('{name}','{login}','{_hashed_password}')")
+    conn.commit()
 
 @app.route('/')
 def index():
     if 'username' in session:
         return f'Logged in as {session["username"]}'
     return 'You are not logged in'
+
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -52,31 +61,44 @@ def login():
         cursor.execute(f"SELECT * FROM {DB_TABLE} WHERE login = '%s'" % login)
         account = cursor.fetchone()
         print(account)
+    return render_template("login.html")
+
 
 @app.route("/index", methods=['POST', 'GET'])
 def register():
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    if request.method == "POST" and 'login' in request.form and 'name' in request.form and 'password' in request.form:
-        name = request.form['name']
-        login = request.form['login']
-        password = request.form['password']
-        session['username'] = request.form['name']
+
+    # Собирание данные с форм и хеширование паса
+
+    if request.method == "POST" and 'login' in request.json and 'name' in request.json and 'password' in request.json:
+        name = request.json['name']
+        login = request.json['login']
+        password = request.json['password']
 
         _hashed_password = generate_password_hash(password)
 
-        cursor.execute(f"SELECT * FROM {DB_TABLE} WHERE login = '%s'" % login)
-        account = cursor.fetchone()
+        # Проверка на валидность имени и логина
+
+        data_response = {
+            "response": True
+        }
+        account = cursor_select("login", login)
         if account:
-            flash('Такой акк уже есть')
+            data_response['response'] = "Куда лезешь он уже есть"
+            return data_response
+
+        account = cursor_select("name", name)
+        if account:
+            data_response['response'] = "Такое имя уже есть "
+            return data_response
         else:
 
-            cursor.execute(f"INSERT INTO {DB_TABLE} (login, name, password) VALUES (%s,%s,%s)", (login, name, _hashed_password))
-            conn.commit()
-            flash("эЭЭ я тЕбя Зарегал жи ест")
-
+            # Занос в бд
+            add_user(name, login, _hashed_password)
+            return data_response
 
     return render_template("index.html")
+
 
 @app.route('/logout')
 def logout():
