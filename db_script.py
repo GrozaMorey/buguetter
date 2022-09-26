a1acec18609f19943e79a34d80d300037d9c63e6
GrozaMorey <yorik294@gmail.com>
2022-09-26 11:40:58 +0300

    added db script file

---
+++ db_script.py
@@ -0,0 +1,19 @@
+import json
+
+import requests
+
+def db_script():
+    link = "http://127.0.0.1:5000/api/register"
+    headers = {
+        "content-type": "application/json"
+    }
+    data = {
+        "login": "Vocir",
+        "name": "sdfsddd",
+        "password": "vcbvcfdg"
+    }
+    json_data = json.dumps(data)
+    requests.post(link, headers=headers, data=json_data)
+
+
+
