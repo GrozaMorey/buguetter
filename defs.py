ee949f8548edfb867cdc10fba2acd024ee54b4c7
GrozaMorey <yorik294@gmail.com>
2022-10-12 18:50:23 +0300

    add pool connection

--- defs.py
+++ defs.py
@@ -3,6 +3,7 @@ import calendar
 import psycopg2.extras
 from app import db, User, Jwt, Post, Tags, db_config, logger
 from flask_jwt_extended import get_jwt_identity
+from psycopg2 import pool
 
 DB_HOST = db_config["DB_HOST"]
 DB_NAME = db_config["DB_NAME"]
@@ -11,22 +12,26 @@ DB_PASS = db_config["DB_PASS"]
 DB_PORT = db_config["DB_PORT"]
 DB_TABLE = db_config["DB_TABLE"]
 
+
 try:
-    logger.info("psycopg2 connect run")
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    logger.info("psycopg2 connect success")
+    postgres_pool = psycopg2.pool.SimpleConnectionPool(1, 20, user=DB_USER, password=DB_PASS,
+                                                       host=DB_HOST, database=DB_NAME)
 except Exception as e:
-    logger.error(f"psycopg2 connect error/ {e}")
+    logger.error(f"Ошибка соединения с бд/ {e}" )
+
 
 
 @logger.catch()
 def cursor_select(column, arg):
     logger.info("cursor_select run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM {DB_TABLE} WHERE {column} = '{arg}'")
         account = cursor.fetchone()
         logger.info("cursor_select success")
+        cursor.close()
+        postgres_pool.putconn(connection)
         return account
     except Exception as e:
         logger.error(f"cursor select error/ args = {column, arg} {e}")
@@ -64,6 +69,8 @@ def add_token_blacklist(jti, token_exp, user_id):
 def get_blocklist_db():
     logger.info("get_blocklist_db run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         cursor.execute("SELECT * FROM invalid_tokens")
         tokens = cursor.fetchall()
         result = [{"id": i[0], "jti": i[1], "date": i[2], "user_id": i[3]} for i in tokens]
@@ -71,8 +78,10 @@ def get_blocklist_db():
         for x in result:
             if int(x["date"]) <= time_stump:
                 cursor.execute(f'DELETE FROM invalid_tokens WHERE id = {x["id"]}')
-                conn.commit()
+                connection.commit()
         logger.info("get_blocklist_db success")
+        cursor.close()
+        postgres_pool.putconn(connection)
         return result
     except Exception as e:
         logger.error(f"get_blocklist_db error/ {e}")
@@ -82,6 +91,8 @@ def get_blocklist_db():
 def add_post(text, user_id, tags_name):
     logger.info("add_post run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         date = time_now()
         post = Post(text, date, user_id)
         db.session.add(post)
@@ -101,14 +112,14 @@ def add_post(text, user_id, tags_name):
                     tags_id_list.append(tags_id[0])
                 else:
                     tags_id_list.append(tags_id[0])
-            print(tags_id_list)
             for i in tags_id_list:
-                print(i)
                 post = Post.query.filter_by(id=post_id[0]).first()
                 tag = Tags.query.filter_by(id=i).first()
                 post.tags.append(tag)
                 db.session.commit()
         logger.info("add_post success")
+        cursor.close()
+        postgres_pool.putconn(connection)
         return True
     except Exception as e:
         logger.error(f"add_post error/ user:{get_jwt_identity} args = {text, user_id, tags_name} {e}")
@@ -133,12 +144,18 @@ def add_tag(text):
 def check_db():
     logger.info("check_db run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         cursor.execute("SELECT version()")
         result = cursor.fetchone()
         if result:
             logger.info("check_db true")
+            cursor.close()
+            postgres_pool.putconn(connection)
             return True
         logger.info("check_db false")
+        cursor.close()
+        postgres_pool.putconn(connection)
         return False
     except Exception as e:
         logger.error(f"check_db error/ {e}")
@@ -148,12 +165,14 @@ def check_db():
 def add_reaction(post_id, reaction, user_id):
     logger.info("add_reaction run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         cursor.execute(f"UPDATE post SET {reaction} = {reaction} + 1 WHERE id = {post_id}")
-        conn.commit()
+        connection.commit()
         cursor.execute(f"UPDATE post SET popularity = cool + shit + angry + nice WHERE id = {post_id}")
         cursor.execute(f"UPDATE post SET karma = cool + nice - shit - angry WHERE id = {post_id}")
         cursor.execute(f"UPDATE post SET total = popularity + karma WHERE id = {post_id}")
-        conn.commit()
+        connection.commit()
 
         cursor.execute(f"SELECT tag_id FROM post_tags WHERE post_id = {post_id}")
         tags = cursor.fetchall()
@@ -173,7 +192,7 @@ def add_reaction(post_id, reaction, user_id):
                     logger.info("add_reaction put + karma for user_tags")
                     cursor.execute(f"UPDATE user_tags SET karma_tag = karma_tag {sign} 1 WHERE user_id = {user_id}"
                                    f" and tag_id = {i[0]}")
-                    conn.commit()
+                    connection.commit()
                 else:
                     logger.info("add_reaction put - karma for user_tags")
                     tag = Tags.query.filter_by(id=i[0]).first()
@@ -181,8 +200,10 @@ def add_reaction(post_id, reaction, user_id):
                     db.session.commit()
                     cursor.execute(f"UPDATE user_tags SET karma_tag = karma_tag {sign} 1 WHERE user_id = {user_id}"
                                    f" and tag_id = {i[0]}")
-                    conn.commit()
+                    connection.commit()
             logger.info("add_reaction success")
+            cursor.close()
+            postgres_pool.putconn(connection)
             return True
     except Exception as e:
         logger.error(f"add_reaction error/ user:{user_id} {e}")
@@ -193,6 +214,8 @@ def add_reaction(post_id, reaction, user_id):
 def get_seen_post(user_id):
     logger.info("get_seen_post run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         # Список постов который видел юзер
         cursor.execute(f"SELECT * FROM user_post WHERE user_id = {user_id}")
         result = cursor.fetchall()
@@ -203,6 +226,8 @@ def get_seen_post(user_id):
             if i[2] <= date:
                 cursor.execute(f"DELETE FROM user_post WHERE user_id = {i[0]} and post_id = {i[1]}")
                 logger.info("get_seen_post run")
+                cursor.close()
+                postgres_pool.putconn(connection)
         return post_id
     except Exception as e:
         logger.error(f"get_seen_post error/ user:{get_jwt_identity} args = {user_id} {e}")
@@ -212,6 +237,8 @@ def get_seen_post(user_id):
 def get_feed(post_id, user_id):
     logger.info("get_feed run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         # добавление поста в игнор лист
         if post_id is not False:
             user = User.query.filter_by(id=user_id).first()
@@ -222,7 +249,7 @@ def get_feed(post_id, user_id):
                 cursor.execute(f"SELECT date FROM post WHERE id = {i}")
                 result = cursor.fetchone()
                 cursor.execute(f"UPDATE user_post SET date = {result[0]} + 604800 WHERE post_id = {i}")
-                conn.commit()
+                connection.commit()
 
         # вывод постов по тоталу и формирование словаря содержащий карму тега
         date = time_now()
@@ -263,14 +290,20 @@ def get_feed(post_id, user_id):
             data[f"{num}"] = i
             num += 1
         logger.info("get_feed success")
+        cursor.close()
+        postgres_pool.putconn(connection)
         return data
     except Exception as e:
         logger.error(f"get_feed error/ user:{user_id} args = {post_id} {e}")
 
 
 def get_user_data(user_id):
+    connection = postgres_pool.getconn()
+    cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM users WHERE id = {user_id}")
     result = cursor.fetchone()
+    cursor.close()
+    postgres_pool.putconn(connection)
     return {"user_id": result[0], "name": result[2]}
 
 
@@ -303,6 +336,8 @@ def time_now():
 def get_post_user(user_id):
     logger.info("get_post_user run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         data = {}
         cursor.execute(f"SELECT * FROM post WHERE user_id = {user_id}")
         result = cursor.fetchall()
@@ -318,6 +353,8 @@ def get_post_user(user_id):
                 "angry": i[6],
                 "nice": i[10]}
             }
+            cursor.close()
+            postgres_pool.putconn(connection)
         return data
     except Exception as e:
         logger.error(f"get_post_user error/ user:{user_id} {e}")
@@ -326,6 +363,8 @@ def get_post_user(user_id):
 def get_likes(user_id):
     logger.info("get_likes run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         cursor.execute(f"SELECT post_id FROM user_post_likes WHERE user_id = {user_id}")
         post_id = []
         data = {}
@@ -347,6 +386,8 @@ def get_likes(user_id):
                 "nice": i[10]}
             }
         logger.info("get_likes success")
+        cursor.close()
+        postgres_pool.putconn(connection)
         return data
     except Exception as e:
         logger.error(f"get_likes error/ user:{user_id} {e}")
@@ -355,8 +396,12 @@ def get_likes(user_id):
 def get_another_user_data(name):
     logger.info("get_another_user_data run")
     try:
+        connection = postgres_pool.getconn()
+        cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM users WHERE name = '{name}'")
         result = cursor.fetchone()
+        cursor.close()
+        postgres_pool.putconn(connection)
         return {"user_id": result[0]}
     except Exception as e:
         logger.error(f"get_another_user_data error/args = {name} {e}")
